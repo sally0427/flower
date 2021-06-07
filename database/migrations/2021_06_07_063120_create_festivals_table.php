02199b5a202c5cef0d043d38bb6a6dc86a963b6b
unknown <ee861027@gmail.com>
2021-06-07 14:40:15 +0800

    add migration festival & meaning

---
+++ database/migrations/2021_06_07_063120_create_festivals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFestivalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('festivals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('date');
+            $table->string('flower');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('festivals');
+    }
+}
